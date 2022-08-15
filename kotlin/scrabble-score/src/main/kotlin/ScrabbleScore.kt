f25cd184480d36daad09aee04a04d4ea4364d2ac
Frank Blendinger <frank.blendinger@methodpark.de>
2022-08-15 17:37:48 +0200

    Add Kotlin Scrabble Score solution

--- kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,10 +1,36 @@
 object ScrabbleScore {
 
-    fun scoreLetter(c: Char): Int {
-        TODO("Implement this function to complete the task")
+    private val scores = hashMapOf<Char, Int>()
+
+    init {
+        for (c in listOf('a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't')) {
+            scores[c] = 1
+        }
+        for (c in listOf('d', 'g')) {
+            scores[c] = 2
+        }
+        for (c in listOf('b', 'c', 'm', 'p')) {
+            scores[c] = 3
+        }
+        for (c in listOf('f', 'h', 'v', 'w', 'y')) {
+            scores[c] = 4
+        }
+
+        scores['k'] = 5
+
+        for (c in listOf('j', 'x')) {
+            scores[c] = 8
+        }
+        for (c in listOf('q', 'z')) {
+            scores[c] = 10
+        }
+    }
+
+    private fun scoreLetter(c: Char): Int {
+        return scores.getValue(c.lowercaseChar())
     }
 
     fun scoreWord(word: String): Int {
-        TODO("Implement this function to complete the task")
+        return word.fold(0) { acc, c -> acc + scoreLetter(c) }
     }
 }
