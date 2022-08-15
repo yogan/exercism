116025b24b237d229ea6ba9101318e04955e0465
Frank Blendinger <frank.blendinger@methodpark.de>
2022-08-15 14:20:13 +0200

    Replace explicit throw with require()

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,8 +1,8 @@
 object Hamming {
 
     fun compute(leftStrand: String, rightStrand: String): Int {
-        if (leftStrand.length != rightStrand.length) {
-            throw IllegalArgumentException("left and right strands must be of equal length")
+        require(leftStrand.length == rightStrand.length) {
+            "left and right strands must be of equal length"
         }
 
         return (leftStrand.toCharArray().toList() zip //
