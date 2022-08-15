c9d0a3342eb732039865be34bbc7a359932366a8
Frank Blendinger <frank.blendinger@methodpark.de>
2022-08-15 11:53:40 +0200

    Add solution for Kotlin Two Fer

--- kotlin/two-fer/src/main/kotlin/TwoFer.kt
+++ kotlin/two-fer/src/main/kotlin/TwoFer.kt
@@ -1,3 +1,3 @@
-fun twofer(name: String): String {
-    TODO("Implement the function to complete the task")
+fun twofer(name: String = "you"): String {
+    return "One for $name, one for me."
 }
--- kotlin/two-fer/src/test/kotlin/TwoFerTest.kt
+++ kotlin/two-fer/src/test/kotlin/TwoFerTest.kt
@@ -1,6 +1,6 @@
+import kotlin.test.assertEquals
 import org.junit.Ignore
 import org.junit.Test
-import kotlin.test.assertEquals
 
 class TwoFerTest {
 
@@ -10,21 +10,17 @@ class TwoFerTest {
     }
 
     @Test
-    @Ignore
     fun aNameGiven() {
         assertEquals("One for Alice, one for me.", twofer("Alice"))
     }
 
     @Test
-    @Ignore
     fun anotherNameGiven() {
         assertEquals("One for Bob, one for me.", twofer("Bob"))
     }
 
     @Test
-    @Ignore
     fun emptyStringGiven() {
         assertEquals("One for , one for me.", twofer(""))
     }
-
 }
