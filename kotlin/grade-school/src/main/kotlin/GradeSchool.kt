45261386f01f68c288c09c43e150fdf0a92406ac
Frank Blendinger <frank.blendinger@methodpark.de>
2022-08-18 09:49:43 +0200

    Add Kotlin Grade School exercise with solution

---
+++ kotlin/grade-school/src/main/kotlin/GradeSchool.kt
@@ -0,0 +1,24 @@
+class School {
+
+    private val roster = mutableMapOf<Int, MutableList<String>>().withDefault { mutableListOf() }
+
+    fun add(student: String, grade: Int) {
+        // FIXME: Why do we have to get the list, add something, and write it back?
+        // Why does this ↓ not work?
+        // roster.getValue(grade).add(student)
+
+        var students = roster.getValue(grade)
+        students.add(student)
+        roster[grade] = students
+    }
+
+    fun grade(grade: Int): List<String> {
+        return roster.getValue(grade).sorted()
+    }
+
+    fun roster(): List<String> {
+        val allStudents = mutableListOf<String>()
+        roster.toSortedMap().values.forEach { grade -> allStudents.addAll(grade.sorted()) }
+        return allStudents
+    }
+}
