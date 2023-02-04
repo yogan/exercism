cb7c999df2d28557e2a3aafd3370bd8a5728a8d5
Frank Blendinger <yogan@zogan.de>
2023-02-04 19:19:01 +0100

    Extract constants to module attributes

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,6 +1,9 @@
 defmodule FreelancerRates do
+  @hours_per_day 8.0
+  @days_per_month 22
+
   def daily_rate(hourly_rate) do
-    hourly_rate * 8.0
+    hourly_rate * @hours_per_day
   end
 
   def apply_discount(before_discount, discount) do
@@ -8,7 +11,7 @@ defmodule FreelancerRates do
   end
 
   def monthly_rate(hourly_rate, discount) do
-    price = daily_rate(hourly_rate) * 22
+    price = daily_rate(hourly_rate) * @days_per_month
     apply_discount(price, discount) |> ceil
   end
 
