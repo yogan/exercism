b679efd1c9f379a79b7c799937b032ab90417a84
Frank Blendinger <yogan@zogan.de>
2023-02-04 19:17:28 +0100

    Solve Elixir Freelancer Rates exercise

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,19 @@
 defmodule FreelancerRates do
   def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
+    hourly_rate * 8.0
   end
 
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
+    before_discount - before_discount * discount / 100
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    price = daily_rate(hourly_rate) * 22
+    apply_discount(price, discount) |> ceil
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    per_day = apply_discount(daily_rate(hourly_rate), discount)
+    Float.floor(budget / per_day, 1)
   end
 end
