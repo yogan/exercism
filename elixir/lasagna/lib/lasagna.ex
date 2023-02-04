2fb895d2d2c787f6e0c6ec5767f2660d4ab3dd8f
Frank Blendinger <yogan@zogan.de>
2023-02-04 15:13:29 +0100

    Solve Elixir Lasagna exercise

--- elixir/lasagna/lib/lasagna.ex
+++ elixir/lasagna/lib/lasagna.ex
@@ -1,11 +1,17 @@
 defmodule Lasagna do
-  # Please define the 'expected_minutes_in_oven/0' function
+  def expected_minutes_in_oven(), do: 40
 
-  # Please define the 'remaining_minutes_in_oven/1' function
+  def remaining_minutes_in_oven(min) do
+    expected_minutes_in_oven() - min
+  end
 
-  # Please define the 'preparation_time_in_minutes/1' function
+  def preparation_time_in_minutes(layers) do
+    layers * 2
+  end
 
-  # Please define the 'total_time_in_minutes/2' function
+  def total_time_in_minutes(layers, oven_time) do
+    preparation_time_in_minutes(layers) + oven_time
+  end
 
-  # Please define the 'alarm/0' function
+  def alarm(), do: "Ding!"
 end
