bc9eaa8c177266ac38d54bed204d8b4fe3a107d7
Frank Blendinger <yogan@zogan.de>
2023-02-05 13:07:10 +0100

    Simplify cond checks

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,18 +1,13 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
     cond do
-      [level, legacy?] == [0, true]  -> :unknown
-      [level, legacy?] == [0, false] -> :trace
-
-      [level, legacy?] == [5, true]  -> :unknown
-      [level, legacy?] == [5, false] -> :fatal
-
-      level == 1                     -> :debug
-      level == 2                     -> :info
-      level == 3                     -> :warning
-      level == 4                     -> :error
-
-      true                           -> :unknown
+      level == 0 and not legacy? -> :trace
+      level == 1                 -> :debug
+      level == 2                 -> :info
+      level == 3                 -> :warning
+      level == 4                 -> :error
+      level == 5 and not legacy? -> :fatal
+      true                       -> :unknown
     end
   end
 
@@ -20,12 +15,10 @@ defmodule LogLevel do
     label = to_label(level, legacy?)
 
     cond do
-      label == :error or label == :fatal    -> :ops
-
-      [label, legacy?] == [:unknown, true]  -> :dev1
-      [label, legacy?] == [:unknown, false] -> :dev2
-
-      true                                  -> false
+      label in [:error, :fatal]         -> :ops
+      label == :unknown and legacy?     -> :dev1
+      label == :unknown and not legacy? -> :dev2
+      true                              -> false
     end
   end
 end
