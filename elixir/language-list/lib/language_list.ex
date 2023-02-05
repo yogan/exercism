9f9a6fe598982ed663aa39733c2a16ef68b92dde
Frank Blendinger <yogan@zogan.de>
2023-02-05 12:17:57 +0100

    Solve Elixir Language List exercise

--- elixir/language-list/lib/language_list.ex
+++ elixir/language-list/lib/language_list.ex
@@ -1,25 +1,29 @@
 defmodule LanguageList do
   def new() do
-    # Please implement the new/0 function
+    []
   end
 
   def add(list, language) do
-    # Please implement the add/2 function
+    [language | list]
   end
 
+  # TODO: support empty list via pattern matching
   def remove(list) do
-    # Please implement the remove/1 function
+    [_head | tail] = list
+    tail
   end
 
   def first(list) do
-    # Please implement the first/1 function
+    [head | _tail] = list
+    head
   end
 
+  # TODO: just for fun, build a recursive solution
   def count(list) do
-    # Please implement the count/1 function
+    length(list)
   end
 
   def functional_list?(list) do
-    # Please implement the functional_list?/1 function
+    "Elixir" in list
   end
 end
