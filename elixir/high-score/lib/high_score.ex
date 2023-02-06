650d4da1decd9eb6fb63c78a697487a75af3524a
Frank Blendinger <yogan@zogan.de>
2023-02-06 17:19:24 +0100

    Solve Elixir High Score exercise

--- elixir/high-score/lib/high_score.ex
+++ elixir/high-score/lib/high_score.ex
@@ -1,25 +1,23 @@
 defmodule HighScore do
-  def new() do
-    # Please implement the new/0 function
-  end
+  def new(), do: %{}
 
-  def add_player(scores, name, score) do
-    # Please implement the add_player/3 function
+  def add_player(scores, name, score \\ 0) do
+    Map.put(scores, name, score)
   end
 
   def remove_player(scores, name) do
-    # Please implement the remove_player/2 function
+    Map.delete(scores, name)
   end
 
   def reset_score(scores, name) do
-    # Please implement the reset_score/2 function
+    add_player(scores, name)
   end
 
   def update_score(scores, name, score) do
-    # Please implement the update_score/3 function
+    Map.update(scores, name, score, fn cur -> cur + score end)
   end
 
   def get_players(scores) do
-    # Please implement the get_players/1 function
+    Map.keys(scores)
   end
 end
