6fe08c3d99bc5b7fef8db9b2ae99a721c2c057cc
Frank Blendinger <yogan@zogan.de>
2023-02-10 11:51:51 +0100

    Solve Elixir German Sysadmin exercise

--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -1,10 +1,28 @@
 defmodule Username do
-  def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
+  defguard is_lowercase_letter(charcode) when ?a <= charcode and charcode <= ?z
+  defguard is_underscore(charcode) when charcode == ?_
+  defguard is_umlaut(charcode) when charcode in [?ä, ?ö, ?ü, ?ß]
 
-    # Please implement the sanitize/1 function
+  defp keep?(charcode) when is_lowercase_letter(charcode), do: true
+  defp keep?(charcode) when is_underscore(charcode), do: true
+  defp keep?(charcode) when is_umlaut(charcode), do: true
+  defp keep?(_charcode), do: false
+
+  defp expand_umlaut(charcode) do
+    case charcode do
+      ?ä -> 'ae'
+      ?ö -> 'oe'
+      ?ü -> 'ue'
+      ?ß -> 'ss'
+      _ -> [charcode]
+    end
+  end
+
+  def sanitize([]), do: []
+  def sanitize([h | t]) do
+    case keep?(h) do
+      true -> expand_umlaut(h) ++ sanitize(t)
+      false -> sanitize(t)
+    end
   end
 end
