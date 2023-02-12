a4bd1853f610763b500e4a6d82a2e5a191c55067
Frank Blendinger <yogan@zogan.de>
2023-02-12 17:16:05 +0100

    Solve JavaScript House exercise

--- javascript/house/house.js
+++ javascript/house/house.js
@@ -1,14 +1,42 @@
-//
-// This is only a SKELETON file for the 'House' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class House {
-  static verse() {
-    throw new Error('Remove this statement and implement this function');
+  static parts = [
+    ['house', 'Jack built.'],
+    ['malt', 'lay in'],
+    ['rat', 'ate'],
+    ['cat', 'killed'],
+    ['dog', 'worried'],
+    ['cow with the crumpled horn', 'tossed'],
+    ['maiden all forlorn', 'milked'],
+    ['man all tattered and torn', 'kissed'],
+    ['priest all shaven and shorn', 'married'],
+    ['rooster that crowed in the morn', 'woke'],
+    ['farmer sowing his corn', 'kept'],
+    ['horse and the hound and the horn', 'belonged to'],
+  ];
+
+  static firstLine(v) {
+    return `This is the ${House.parts[v - 1][0]}`
+      + `${v === 1 ? ` that ${House.parts[0][1]}` : ''}`;
   }
 
-  static verses() {
-    throw new Error('Remove this statement and implement this function');
+  static line(v) {
+    const prev = House.verse(v - 1);
+    prev[0] = prev[0].replace('This is', `that ${House.parts[v - 1][1]}`);
+    return [House.firstLine(v), ...prev];
+  }
+
+  static verse(v) {
+    return v === 1
+      ? [House.firstLine(v)]
+      : House.line(v)
+  }
+
+  static verses(start, end) {
+    const lines = []
+    for (let i = start; i <= end; i++) {
+      lines.push(...House.verse(i));
+      if (i !== end) { lines.push("") }
+    }
+    return lines;
   }
 }
--- javascript/house/house.spec.js
+++ javascript/house/house.spec.js
@@ -6,7 +6,7 @@ describe('House', () => {
     expect(House.verse(1)).toEqual(lyrics);
   });
 
-  xtest('verse two - the malt that lay', () => {
+  test('verse two - the malt that lay', () => {
     const lyrics = [
       'This is the malt',
       'that lay in the house that Jack built.',
@@ -14,7 +14,7 @@ describe('House', () => {
     expect(House.verse(2)).toEqual(lyrics);
   });
 
-  xtest('verse three - the rat that ate', () => {
+  test('verse three - the rat that ate', () => {
     const lyrics = [
       'This is the rat',
       'that ate the malt',
@@ -23,7 +23,7 @@ describe('House', () => {
     expect(House.verse(3)).toEqual(lyrics);
   });
 
-  xtest('verse four - the cat that killed', () => {
+  test('verse four - the cat that killed', () => {
     const lyrics = [
       'This is the cat',
       'that killed the rat',
@@ -33,7 +33,7 @@ describe('House', () => {
     expect(House.verse(4)).toEqual(lyrics);
   });
 
-  xtest('verse five - the dog that worried', () => {
+  test('verse five - the dog that worried', () => {
     const lyrics = [
       'This is the dog',
       'that worried the cat',
@@ -44,7 +44,7 @@ describe('House', () => {
     expect(House.verse(5)).toEqual(lyrics);
   });
 
-  xtest('verse six - the cow with the crumpled horn', () => {
+  test('verse six - the cow with the crumpled horn', () => {
     const lyrics = [
       'This is the cow with the crumpled horn',
       'that tossed the dog',
@@ -56,7 +56,7 @@ describe('House', () => {
     expect(House.verse(6)).toEqual(lyrics);
   });
 
-  xtest('verse seven - the maiden all forlorn', () => {
+  test('verse seven - the maiden all forlorn', () => {
     const lyrics = [
       'This is the maiden all forlorn',
       'that milked the cow with the crumpled horn',
@@ -69,7 +69,7 @@ describe('House', () => {
     expect(House.verse(7)).toEqual(lyrics);
   });
 
-  xtest('verse eight - the man all tattered and torn', () => {
+  test('verse eight - the man all tattered and torn', () => {
     const lyrics = [
       'This is the man all tattered and torn',
       'that kissed the maiden all forlorn',
@@ -83,7 +83,7 @@ describe('House', () => {
     expect(House.verse(8)).toEqual(lyrics);
   });
 
-  xtest('verse nine - the priest all shaven and shorn', () => {
+  test('verse nine - the priest all shaven and shorn', () => {
     const lyrics = [
       'This is the priest all shaven and shorn',
       'that married the man all tattered and torn',
@@ -98,7 +98,7 @@ describe('House', () => {
     expect(House.verse(9)).toEqual(lyrics);
   });
 
-  xtest('verse ten - the rooster that crowed in the morn', () => {
+  test('verse ten - the rooster that crowed in the morn', () => {
     const lyrics = [
       'This is the rooster that crowed in the morn',
       'that woke the priest all shaven and shorn',
@@ -114,7 +114,7 @@ describe('House', () => {
     expect(House.verse(10)).toEqual(lyrics);
   });
 
-  xtest('verse eleven - the farmer sowing his corn', () => {
+  test('verse eleven - the farmer sowing his corn', () => {
     const lyrics = [
       'This is the farmer sowing his corn',
       'that kept the rooster that crowed in the morn',
@@ -131,7 +131,7 @@ describe('House', () => {
     expect(House.verse(11)).toEqual(lyrics);
   });
 
-  xtest('verse twelve - the horse and the hound and the horn', () => {
+  test('verse twelve - the horse and the hound and the horn', () => {
     const lyrics = [
       'This is the horse and the hound and the horn',
       'that belonged to the farmer sowing his corn',
@@ -149,7 +149,7 @@ describe('House', () => {
     expect(House.verse(12)).toEqual(lyrics);
   });
 
-  xtest('multiple verses', () => {
+  test('multiple verses', () => {
     const startVerse = 4;
     const endVerse = 8;
     const lyrics = [
@@ -191,7 +191,7 @@ describe('House', () => {
     expect(House.verses(startVerse, endVerse)).toEqual(lyrics);
   });
 
-  xtest('full rhyme', () => {
+  test('full rhyme', () => {
     const startVerse = 1;
     const endVerse = 12;
     const lyrics = [
