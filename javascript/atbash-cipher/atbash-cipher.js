38d65004a7bc658e6bec2e78829c4b5aa8631b97
Frank Blendinger <yogan@zogan.de>
2023-02-12 18:11:05 +0100

    Add JavaScript Atbash Cipher exercise

---
+++ javascript/atbash-cipher/atbash-cipher.js
@@ -0,0 +1,7 @@
+export const encode = () => {
+  throw new Error('Remove this statement and implement this function');
+};
+
+export const decode = () => {
+  throw new Error('Remove this statement and implement this function');
+};
