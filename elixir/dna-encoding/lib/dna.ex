8fca6739f0bb516a9217baf303d379f9e20c7a00
Frank Blendinger <yogan@zogan.de>
2023-02-17 16:57:45 +0100

    Solve Elixir DNA Encoding exercise

--- elixir/dna-encoding/lib/dna.ex
+++ elixir/dna-encoding/lib/dna.ex
@@ -1,17 +1,39 @@
 defmodule DNA do
   def encode_nucleotide(code_point) do
-    # Please implement the encode_nucleotide/1 function
+    case (code_point) do
+      ?\s -> 0b0000
+      ?A  -> 0b0001
+      ?C  -> 0b0010
+      ?G  -> 0b0100
+      ?T  -> 0b1000
+    end
   end
 
   def decode_nucleotide(encoded_code) do
-    # Please implement the decode_nucleotide/1 function
+    case (encoded_code) do
+      0b0000 -> ?\s
+      0b0001 -> ?A
+      0b0010 -> ?C
+      0b0100 -> ?G
+      0b1000 -> ?T
+    end
   end
 
   def encode(dna) do
-    # Please implement the encode/1 function
+    encode_rec(dna, <<>>)
+  end
+
+  defp encode_rec([], acc), do: acc
+  defp encode_rec([h | t], acc) do
+    encode_rec(t, <<acc::bitstring, encode_nucleotide(h)::4>>)
   end
 
   def decode(dna) do
-    # Please implement the decode/1 function
+    decode_rec(dna, '')
+  end
+
+  defp decode_rec(<<>>, acc), do: acc
+  defp decode_rec(<<h::4, t::bitstring>>, acc) do
+    decode_rec(t, acc ++ [decode_nucleotide(h)])
   end
 end
