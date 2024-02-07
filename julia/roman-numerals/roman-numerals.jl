0e63a8829137a0649b19ea12c5f1687f8493ccd7
Frank Blendinger <yogan@zogan.de>
2024-02-07 16:35:44 +0100

    Cleanup Roman Numerals Julia solution
    - implicit returns
    - replace list with tuple for table
    - early failure via ∉ for invalid input

--- julia/roman-numerals/roman-numerals.jl
+++ julia/roman-numerals/roman-numerals.jl
@@ -1,53 +1,54 @@
 function to_roman(number)
-    if number ∈ 1:3999
-        return roman_table(number)
-        # return roman_rec(number)
+    if number ∉ 1:3999
+        throw(ErrorException("$number can't be represented in Roman numerals."))
     end
 
-    throw(ErrorException("$number cannot be represented in Roman numerals."))
+    roman_table(number)
+    # roman_rec(number)
 end
 
+# Table-based version
 function roman_table(number)
-    table = [
+    table = (
         ("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"),
         ("X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"),
         ("C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"),
         ("M", "MM", "MMM"),
-    ]
+    )
 
-    return map(
-        (digit, row) -> digit > 0 ? row[digit] : "",
+    map((digit, row) -> digit > 0 ? row[digit] : "",
         digits(number), table) |> reverse |> join
 end
 
+# Alternative, recursive version
 function roman_rec(number)
     if number >= 1000
-        return "M" * roman_rec(number - 1000)
+        "M"  * roman_rec(number - 1000)
     elseif number >= 900
-        return "CM" * roman_rec(number - 900)
+        "CM" * roman_rec(number - 900)
     elseif number >= 500
-        return "D" * roman_rec(number - 500)
+        "D"  * roman_rec(number - 500)
     elseif number >= 400
-        return "CD" * roman_rec(number - 400)
+        "CD" * roman_rec(number - 400)
     elseif number >= 100
-        return "C" * roman_rec(number - 100)
+        "C"  * roman_rec(number - 100)
     elseif number >= 90
-        return "XC" * roman_rec(number - 90)
+        "XC" * roman_rec(number - 90)
     elseif number >= 50
-        return "L" * roman_rec(number - 50)
+        "L"  * roman_rec(number - 50)
     elseif number >= 40
-        return "XL" * roman_rec(number - 40)
+        "XL" * roman_rec(number - 40)
     elseif number >= 10
-        return "X" * roman_rec(number - 10)
+        "X"  * roman_rec(number - 10)
     elseif number >= 9
-        return "IX" * roman_rec(number - 9)
+        "IX" * roman_rec(number - 9)
     elseif number >= 5
-        return "V" * roman_rec(number - 5)
+        "V"  * roman_rec(number - 5)
     elseif number >= 4
-        return "IV" * roman_rec(number - 4)
+        "IV" * roman_rec(number - 4)
     elseif number >= 1
-        return "I" * roman_rec(number - 1)
+        "I"  * roman_rec(number - 1)
     else
-        return ""
+        ""
     end
 end
