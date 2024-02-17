3034bd90d06a4545f8c92da898124edd2a8233f9
Frank Blendinger <yogan@zogan.de>
2024-02-17 18:44:43 +0100

    Add GitHub Action step for .NET (C#/F#) solutions

---
+++ bin/test-dotnet.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd "$1" || exit 1
+
+dirs=$(ls -d -- */)
+exit_code=0
+
+for dir in $dirs; do
+  cd "$dir" || exit 1
+  if ! dotnet test ; then
+    exit_code=1
+  fi
+  cd ..
+done
+
+return $exit_code
