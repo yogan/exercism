99972cb66ce7800779a17143069866bc81e866f3
Frank Blendinger <yogan@zogan.de>
2024-02-17 18:08:08 +0100

    Add GitHub Action step for Crystal solutions

---
+++ bin/test-crystal.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd crystal || exit 1
+
+dirs=$(ls -d -- */)
+exit_code=0
+
+for dir in $dirs; do
+  cd "$dir" || exit 1
+  if ! crystal spec ; then
+    exit_code=1
+  fi
+  cd ..
+done
+
+return $exit_code
