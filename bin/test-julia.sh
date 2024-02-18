cffc7f6d64c7772001a0ff08f78f31c86ac77484
Frank Blendinger <yogan@zogan.de>
2024-02-18 16:23:41 +0100

    Add GitHub Action step for Julia

---
+++ bin/test-julia.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd julia || exit 1
+
+dirs=$(ls -d -- */)
+exit_code=0
+
+for dir in $dirs; do
+  cd "$dir" || exit 1
+  if ! julia runtests.jl ; then
+    exit_code=1
+  fi
+  cd ..
+done
+
+return $exit_code
