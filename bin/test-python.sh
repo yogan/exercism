b1c3b253365c1676140ca1346ff43f8c242f604c
Frank Blendinger <yogan@zogan.de>
2024-02-18 17:16:26 +0100

    Add GitHub Action step for Python

---
+++ bin/test-python.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd python || exit 1
+
+dirs=$(ls -d -- */)
+exit_code=0
+
+for dir in $dirs; do
+  cd "$dir" || exit 1
+  if ! python3 -m pytest -o markers=task ./*test*py ; then
+    exit_code=1
+  fi
+  cd ..
+done
+
+return $exit_code
