5eee319280f9918991ec5009bba2b5648e9bef7e
Frank Blendinger <yogan@zogan.de>
2024-02-19 10:56:31 +0100

    Add GitHub Action step for R

---
+++ bin/test-r.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd r || exit 1
+
+dirs=$(ls -d -- */)
+exit_code=0
+
+for dir in $dirs; do
+  cd "$dir" || exit 1
+  if ! Rscript test*.R ; then
+    exit_code=1
+  fi
+  cd ..
+done
+
+return $exit_code
