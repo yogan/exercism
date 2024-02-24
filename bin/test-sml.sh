0d30e56725e7536fb5234707f2534a695b7dc255
Frank Blendinger <yogan@zogan.de>
2024-02-24 14:13:23 +0100

    Add GitHub Action step for Standard ML

---
+++ bin/test-sml.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd sml || exit 1
+
+dirs=$(ls -d -- */)
+exit_code=0
+
+for dir in $dirs; do
+  cd "$dir" || exit 1
+  if ! poly -q --use test.sml; then
+    exit_code=1
+  fi
+  cd ..
+done
+
+return $exit_code
