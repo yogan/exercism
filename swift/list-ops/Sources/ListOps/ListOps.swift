397d8bf4e93904b8809a9bdcd7984ccc63b2930c
Frank Blendinger <yogan@zogan.de>
2024-02-26 17:22:50 +0100

    Remove ListOps class (Swift)

--- swift/list-ops/Sources/ListOps/ListOps.swift
+++ swift/list-ops/Sources/ListOps/ListOps.swift
@@ -1,88 +1,86 @@
-class ListOps {
-    class func append<T>(_ list1: [T], _ list2: [T]) -> [T] {
-        if list1.isEmpty {
-            return list2
-        }
-
-        return list1[0...0] + append(Array(list1[1...]), list2)
+func append<T>(_ list1: [T], _ list2: [T]) -> [T] {
+    if list1.isEmpty {
+        return list2
     }
 
-    class func concat<T>(_ lists: [T]...) -> [T] {
-        // variadic functions cannot be called with an array directly, and there
-        // is no splat operator, so we have to delegate to a helper function
-        // that takes an array, see https://stackoverflow.com/a/24024740/183582
-        return concat(lists)
+    return list1[0...0] + append(Array(list1[1...]), list2)
+}
+
+func concat<T>(_ lists: [T]...) -> [T] {
+    // variadic functions cannot be called with an array directly, and there
+    // is no splat operator, so we have to delegate to a helper function
+    // that takes an array, see https://stackoverflow.com/a/24024740/183582
+    return concat(lists)
+}
+
+func concat<T>(_ listOfLists: [[T]])-> [T] {
+    if listOfLists.isEmpty {
+        return []
     }
 
-    class func concat<T>(_ listOfLists: [[T]])-> [T] {
-        if listOfLists.isEmpty {
-            return []
-        }
-
-        if listOfLists[0].isEmpty {
-            return concat(Array(listOfLists[1...]))
-        }
-
-        let head: [T] = [listOfLists[0][0]]
-        let rest: [T] = Array(listOfLists[0][1...])
-        let restList: Array<[T]> = Array(listOfLists[1...])
-
-        return head + concat([rest] + restList)
+    if listOfLists[0].isEmpty {
+        return concat(Array(listOfLists[1...]))
     }
 
-    class func filter<T>(_ list: [T], _ predicate: (T) -> Bool) -> [T] {
-        if list.isEmpty {
-            return []
-        }
+    let head: [T] = [listOfLists[0][0]]
+    let rest: [T] = Array(listOfLists[0][1...])
+    let restList: Array<[T]> = Array(listOfLists[1...])
 
-        if predicate(list[0]) {
-            return [list[0]] + filter(Array(list[1...]), predicate)
-        }
+    return head + concat([rest] + restList)
+}
 
-        return filter(Array(list[1...]), predicate)
+func filter<T>(_ list: [T], _ predicate: (T) -> Bool) -> [T] {
+    if list.isEmpty {
+        return []
     }
 
-    class func length<T>(_ list: [T]) -> Int {
-        if list.isEmpty {
-            return 0
-        }
-
-        return 1 + length(Array(list[1...]))
+    if predicate(list[0]) {
+        return [list[0]] + filter(Array(list[1...]), predicate)
     }
 
-    class func map<T>(_ list: [T], _ op: (T) -> T) -> [T] {
-        if list.isEmpty {
-            return []
-        }
+    return filter(Array(list[1...]), predicate)
+}
 
-        return [op(list[0])] + map(Array(list[1...]), op)
+func length<T>(_ list: [T]) -> Int {
+    if list.isEmpty {
+        return 0
     }
 
-    class func foldLeft<T>(_ list: [T], accumulated acc: T, combine op: (T, T) -> T) -> T {
-        if list.isEmpty {
-            return acc
-        }
+    return 1 + length(Array(list[1...]))
+}
 
-        return foldLeft(Array(list[1...]), accumulated: op(acc, list[0]), combine: op)
+func map<T>(_ list: [T], _ op: (T) -> T) -> [T] {
+    if list.isEmpty {
+        return []
     }
 
-    class func foldRight<T>(_ list: [T], accumulated acc: T, combine op: (T, T) -> T) -> T {
-        if list.isEmpty {
-            return acc
-        }
+    return [op(list[0])] + map(Array(list[1...]), op)
+}
 
-        return op(list[0], foldRight(Array(list[1...]), accumulated: acc, combine: op))
+func foldLeft<T>(_ list: [T], accumulated acc: T, combine op: (T, T) -> T) -> T {
+    if list.isEmpty {
+        return acc
     }
 
-    class func reverse<T>(_ list: [T]) -> [T] {
-        return rev(list, [])
+    return foldLeft(Array(list[1...]), accumulated: op(acc, list[0]), combine: op)
+}
+
+func foldRight<T>(_ list: [T], accumulated acc: T, combine op: (T, T) -> T) -> T {
+    if list.isEmpty {
+        return acc
     }
 
-    class func rev<T>(_ list: [T], _ acc: [T]) -> [T] {
-        if list.isEmpty {
-            return acc
-        }
+    return op(list[0], foldRight(Array(list[1...]), accumulated: acc, combine: op))
+}
 
-        return rev(Array(list[1...]), [list[0]] + acc)
+func reverse<T>(_ list: [T]) -> [T] {
+    return rev(list, [])
+}
+
+func rev<T>(_ list: [T], _ acc: [T]) -> [T] {
+    if list.isEmpty {
+        return acc
     }
+
+    return rev(Array(list[1...]), [list[0]] + acc)
 }
