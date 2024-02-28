bc098cc6be7145af51f1dc07e746ab44c46d1b05
Frank Blendinger <yogan@zogan.de>
2024-02-28 19:27:49 +0100

    Add Acronym in Haskell

---
+++ haskell/acronym/src/Acronym.hs
@@ -0,0 +1,25 @@
+module Acronym (abbreviate) where
+
+import Data.Char
+
+abbreviate :: String -> String
+abbreviate xs =
+    map toUpper $ map firstLetter $ words $ unCamel $ dashesToSpaces xs
+
+firstLetter :: String -> Char
+firstLetter [] = error "no empty string pls"
+firstLetter (x:xs) =
+    if isAlpha x
+        then x
+        else firstLetter xs
+
+unCamel :: String -> String
+unCamel [] = []
+unCamel [x] = [x]
+unCamel (x:y:rest) =
+    if isLower x && isUpper y
+        then [x, ' ', y] ++ unCamel rest
+        else [x,      y] ++ unCamel rest
+
+dashesToSpaces :: String -> String
+dashesToSpaces = map $ \c -> if c == '-' then ' ' else c
