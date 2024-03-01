37d6359c0ecb8f72cb73ee5373165ac360781e8d
Frank Blendinger <yogan@zogan.de>
2024-03-01 15:57:52 +0100

    Replace case with guards (Darts in Haskell)

--- haskell/darts/src/Darts.hs
+++ haskell/darts/src/Darts.hs
@@ -1,10 +1,9 @@
 module Darts (score) where
 
 score :: Float -> Float -> Int
-score x y = points
+score x y
+    | distance > 10 = 0
+    | distance > 5  = 1
+    | distance > 1  = 5
+    | otherwise     = 10
     where distance = sqrt (x^(2::Int) + y^(2::Int))
-          points = case distance of
-              _ | distance > 10 -> 0
-              _ | distance > 5  -> 1
-              _ | distance > 1  -> 5
-              _                 -> 10
