14795a29f13441551da162435349812569296329
Frank Blendinger <yogan@zogan.de>
2024-03-04 18:20:50 +0100

    Replace Set.fromList with List.nub (SumOfM/Haskell)

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,9 +1,9 @@
 module SumOfMultiples (sumOfMultiples) where
 
-import Data.Set (fromList)
+import Data.List (nub)
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = sum $ fromList values
+sumOfMultiples factors limit = sum $ nub values
   where
     multiples n = takeWhile (< limit) [n, n * 2 ..]
     values = concatMap multiples $ filter (/= 0) factors
