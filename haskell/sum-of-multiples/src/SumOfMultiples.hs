a0978f1a5fdc1779b350d56a6ae208e8fd851a4a
Frank Blendinger <yogan@zogan.de>
2024-03-04 18:21:46 +0100

    Shorten sumOfMultiples function (SumOfM / Haskell)

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -3,7 +3,7 @@ module SumOfMultiples (sumOfMultiples) where
 import Data.List (nub)
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = sum $ nub values
+sumOfMultiples factors limit =
+  sum $ nub $ concatMap multiples $ filter (/= 0) factors
   where
     multiples n = takeWhile (< limit) [n, n * 2 ..]
-    values = concatMap multiples $ filter (/= 0) factors
