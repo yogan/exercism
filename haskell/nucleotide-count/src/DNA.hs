252a5dc130bd6ed761a86f359403487966ce851e
Frank Blendinger <yogan@zogan.de>
2024-03-04 13:17:06 +0100

    Remove defaults (Nucleotide Count in Haskell)

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -8,10 +8,9 @@ data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
 nucleotideCounts dna = case counts of
-  Right xs -> Right $ Map.fromList $ defaults ++ xs
+  Right xs -> Right $ Map.fromList xs
   Left err -> Left err
   where
-    defaults = [(A, 0), (C, 0), (G, 0), (T, 0)]
     counts = mapM toNucleatideCount $ group $ sort dna
 
     toNucleatideCount :: String -> Either String (Nucleotide, Int)
