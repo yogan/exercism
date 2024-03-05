b13c5992208f5331954095dab1670411d975b17a
Frank Blendinger <yogan@zogan.de>
2024-03-05 21:17:57 +0100

    Add Hello World in Groovy

---
+++ bin/groovy-gradle.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64 sh ./gradlew "$@"
---
+++ bin/test-groovy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd groovy || exit 1
+
+dirs=$(ls -d -- */)
+exit_code=0
+
+for dir in $dirs; do
+  cd "$dir" || exit 1
+  if ! ../../bin/groovy-gradle.sh test ; then
+    exit_code=1
+  fi
+  cd ..
+done
+
+return $exit_code
