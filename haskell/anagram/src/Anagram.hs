e1284de89ae2d371b37eb282a31411430631ff1c
Frank Blendinger <yogan@zogan.de>
2024-03-08 17:30:02 +0100

    Simplify by using sort (Anagram / Haskell)

--- haskell/anagram/src/Anagram.hs
+++ haskell/anagram/src/Anagram.hs
@@ -1,26 +1,13 @@
 module Anagram (anagramsFor) where
 
 import Data.Char (toLower)
+import Data.List (sort)
 
 anagramsFor :: String -> [String] -> [String]
-anagramsFor xs yss = filter (isAnagram xs) noDupes
+anagramsFor s = filter (isAnagram s)
   where
-    noDupes = filter (not . sameWord) yss
-    sameWord ys = map toLower xs == map toLower ys
-
-isAnagram :: String -> String -> Bool
-isAnagram [] [] = True
-isAnagram (_ : _) [] = False
-isAnagram [] (_ : _) = False
-isAnagram (x : xs) ys
-  | lx `elem` lys = isAnagram xs (dropFirst lx lys)
-  | otherwise = False
-  where
-    lx = toLower x
-    lys = map toLower ys
-
-dropFirst :: Char -> String -> String
-dropFirst _ [] = []
-dropFirst c (x : xs)
-  | c == x = xs
-  | otherwise = x : dropFirst c xs
+    isAnagram :: String -> String -> Bool
+    isAnagram xs ys = lxs /= lys && sort lxs == sort lys
+      where
+        lxs = map toLower xs
+        lys = map toLower ys
