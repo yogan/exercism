9e368f338df54b0a5770b2e350fdf332d6134155
Frank Blendinger <yogan@zogan.de>
2024-03-18 10:22:31 +0100

    Use OpenJDK 19 for Java tests

--- bin/test-java.sh
+++ bin/test-java.sh
@@ -4,16 +4,16 @@ cd java || exit 1
 dirs=$(ls -d -- */)
 exit_code=0
 
-# Exercism gradle stuff fails with OpenJDK 22, but runs with 21 (AoC Docker
-# image with Ubuntu 23.10) and 19 (inobook-v2 WSL with Ubuntu 22.04 LTS).
-if [ -d /usr/lib/jvm/java-21-openjdk-amd64 ]; then
-	export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
-	echo using JAVA_HOME=$JAVA_HOME
-elif [ -d /usr/lib/jvm/java-19-openjdk-amd64 ]; then
+# Exercism gradle stuff fails with OpenJDK 22, but runs with 19 and 21 (tested
+# on inobook-v2 WSL with Ubuntu 22.04 LTS and AoC Docker image with Ubuntu 23.10
+# respectively).
+# However, installing OpenJDK 21 in the Docker image broke the Scala/sbt stuff,
+# so that was removed and we are sticking with OpenJDK 19 for now.
+if [ -d /usr/lib/jvm/java-19-openjdk-amd64 ]; then
 	export JAVA_HOME=/usr/lib/jvm/java-19-openjdk-amd64
 	echo using JAVA_HOME=$JAVA_HOME
 else
-	echo "No OpenJDK 19 or 21 found"
+	echo "No OpenJDK 19 found"
 	exit 1
 fi
 
