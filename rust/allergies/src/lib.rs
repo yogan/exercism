8815560b9307ffec5b2296d75b3a3180010f4698
Frank Blendinger <yogan@zogan.de>
2024-03-27 14:46:04 +0100

    Add Allergies in Rust

---
+++ rust/allergies/src/lib.rs
@@ -0,0 +1,40 @@
+pub struct Allergies {
+    score: u32,
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+pub enum Allergen {
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128,
+}
+
+impl Allergies {
+    pub fn new(score: u32) -> Self {
+        Allergies { score }
+    }
+
+    pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
+        self.score & *allergen as u32 != 0
+    }
+
+    pub fn allergies(&self) -> Vec<Allergen> {
+        vec![
+            Allergen::Eggs,
+            Allergen::Peanuts,
+            Allergen::Shellfish,
+            Allergen::Strawberries,
+            Allergen::Tomatoes,
+            Allergen::Chocolate,
+            Allergen::Pollen,
+            Allergen::Cats,
+        ].into_iter()
+            .filter(|allergen| self.is_allergic_to(allergen))
+            .collect()
+    }
+}
