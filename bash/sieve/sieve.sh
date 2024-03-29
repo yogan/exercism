1f7813bc642ae24606cda26c949fd06970cf759f
Frank Blendinger <yogan@zogan.de>
2024-03-29 13:01:55 +0100

    Use declare to get rid of quoting (Sieve / Bash)

--- bash/sieve/sieve.sh
+++ bash/sieve/sieve.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
+declare -a numbers primes multiples
+declare -i limit m n prime
+
 limit=$1
-mapfile -t numbers < <(seq 0 "$limit")
+mapfile -t numbers < <(seq 0 $limit)
 
 for n in "${!numbers[@]}"; do
-    prime=${numbers[$n]}
-    if [ "$prime" -lt 2 ]; then
+    prime=${numbers[n]}
+    if [ $prime -lt 2 ]; then
         continue
     fi
     primes+=("$prime")
-    mapfile -t multiples < <(seq $((2 * n)) "$n" "$limit")
+    mapfile -t multiples < <(seq $((2 * n)) $n $limit)
     for m in "${multiples[@]}"; do
-        numbers["$m"]=0
+        numbers[m]=0
     done
 done
 
