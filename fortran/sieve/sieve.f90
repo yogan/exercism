89b7bca6954ac24c500db110b39cacb0dbad0070
Frank Blendinger <yogan@zogan.de>
2024-03-31 16:32:34 +0200

    Simplify Sieve in Fortran

--- fortran/sieve/sieve.f90
+++ fortran/sieve/sieve.f90
@@ -5,39 +5,22 @@ contains
 
    function primes(limit) result(array)
       integer, intent(in) :: limit
+      integer, allocatable :: array(:)
       integer :: i, j
-      integer, allocatable :: array(:), sieve(:)
+      logical :: is_prime(limit)
 
-      allocate (sieve(limit))
-
-      do i = 2, limit
-         sieve(i) = i
-      end do
+      is_prime = [(.true., i=1, limit)]
+      is_prime(1) = .false.
 
       do i = 2, limit
-         if (sieve(i) /= 0) then
+         if (is_prime(i)) then
             do j = i + i, limit, i
-               sieve(j) = 0
+               is_prime(j) = .false.
             end do
          end if
       end do
 
-      j = 0
-      do i = 2, limit
-         if (sieve(i) /= 0) then
-            j = j + 1
-         end if
-      end do
-
-      allocate (array(j))
-
-      j = 0
-      do i = 2, limit
-         if (sieve(i) /= 0) then
-            j = j + 1
-            array(j) = sieve(i)
-         end if
-      end do
+      array = pack([(i, i=1, limit)], is_prime)
 
    end function primes
 
