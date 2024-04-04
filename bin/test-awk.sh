b3463c5fa138b4b93e561e341266bae058926ee9
Frank Blendinger <yogan@zogan.de>
2024-04-04 16:33:01 +0200

    Add Hello World in AWK

---
+++ bin/test-awk.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd awk || exit 1
+
+dirs=$(ls -d -- */)
+exit_code=0
+
+for dir in $dirs; do
+    cd "$dir" || exit 1
+    if ! bats ./*.bats; then
+        exit_code=1
+    fi
+    cd ..
+done
+
+return $exit_code
