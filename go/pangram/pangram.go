149c98dd9c184451eee0114b91087e500d0d2fe3
Frank Blendinger <yogan@zogan.de>
2024-04-24 12:05:20 +0200

    Use rune map instead of string (Panagram / Go)

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -4,15 +4,16 @@ import (
 	"strings"
 )
 
-func IsPangram(input string) bool {
-	var chars = strings.Split(strings.ToLower(input), "")
-	var charMap = make(map[string]bool)
+const NumberOfLetters = len("abcdefghijklmnopqrstuvwxyz")
 
-	for _, char := range chars {
-		if char >= "a" && char <= "z" {
+func IsPangram(input string) bool {
+	var charMap = map[rune]bool{}
+
+	for _, char := range strings.ToLower(input) {
+		if char >= 'a' && char <= 'z' {
 			charMap[char] = true
 		}
 	}
 
-	return len(charMap) == len("abcdefghijklmnopqrstuvwxyz")
+	return len(charMap) == NumberOfLetters
 }
