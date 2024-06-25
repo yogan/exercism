add3abb0e380446e7d1ef8daa5c7913f9f5a6b96
Frank Blendinger <yogan@zogan.de>
2024-06-25 13:02:45 +0200

    Add bin/cleanup.sh script to clean up CI workspace

---
+++ bin/cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <source> <destination>"
+    exit 1
+fi
+
+lang=$1
+logfile=$2
+
+git clean -fxd
+echo "$lang done" >> "$logfile"
+df -h | grep root | tee -a "$logfile"
