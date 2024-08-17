a12ea97fcd4ae8fd537fa8e943269880d50bb234
Frank Blendinger <yogan@zogan.de>
2024-08-17 18:34:45 +0200

    Code cleanup (Minesweeper / Haskell)

--- haskell/minesweeper/src/Minesweeper.hs
+++ haskell/minesweeper/src/Minesweeper.hs
@@ -4,26 +4,33 @@ type Board = [String]
 
 type Pos = (Int, Int)
 
+type Size = (Int, Int)
+
 annotate :: Board -> Board
 annotate [] = []
-annotate board = [[countMines (i, j) board m n | j <- [0 .. n]] | i <- [0 .. m]]
+annotate board = [[countMines (i, j) board | j <- [0 .. n]] | i <- [0 .. m]]
   where
-    m = length board - 1
-    n = length (head board) - 1
+    (m, n) = size board
 
-countMines :: Pos -> Board -> Int -> Int -> Char
-countMines (i, j) board m n
+countMines :: Pos -> Board -> Char
+countMines (i, j) board
   | isMine (i, j) board = '*'
-  | otherwise = case countMines' (i, j) board m n of
+  | otherwise = case countMines' (i, j) board of
       0 -> ' '
       x -> head (show x)
 
-countMines' :: Pos -> Board -> Int -> Int -> Int
-countMines' (i, j) board m n = length mines
+countMines' :: Pos -> Board -> Int
+countMines' (i, j) board = length $ filter (`isMine` board) indices
   where
-    indices = [(i + di, j + dj) | di <- [-1, 0, 1], dj <- [-1, 0, 1]]
-    validIndices = [(k, l) | (k, l) <- indices, k >= 0 && k <= m && l >= 0 && l <= n]
-    mines = filter (`isMine` board) validIndices
+    range = [-1 .. 1]
+    indices = [(i + di, j + dj) | di <- range, dj <- range]
 
 isMine :: Pos -> Board -> Bool
-isMine (i, j) board = board !! i !! j == '*'
+isMine (i, j) board
+  | i < 0 || i > m || j < 0 || j > n = False
+  | otherwise = board !! i !! j == '*'
+  where
+    (m, n) = size board
+
+size :: Board -> Size
+size board = (length board - 1, length (head board) - 1)
