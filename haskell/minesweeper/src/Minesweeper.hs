b071d919498d9afc8fad8d1526a87dd3c618b07d
Frank Blendinger <yogan@zogan.de>
2024-08-17 18:38:05 +0200

    Improve parameter order (Minesweeper / Haskell)

--- haskell/minesweeper/src/Minesweeper.hs
+++ haskell/minesweeper/src/Minesweeper.hs
@@ -8,25 +8,25 @@ type Size = (Int, Int)
 
 annotate :: Board -> Board
 annotate [] = []
-annotate board = [[countMines (i, j) board | j <- [0 .. n]] | i <- [0 .. m]]
+annotate board = [[countMines board (i, j) | j <- [0 .. n]] | i <- [0 .. m]]
   where
     (m, n) = size board
 
-countMines :: Pos -> Board -> Char
-countMines (i, j) board
-  | isMine (i, j) board = '*'
-  | otherwise = case countMines' (i, j) board of
+countMines :: Board -> Pos -> Char
+countMines board (i, j)
+  | isMine board (i, j) = '*'
+  | otherwise = case countMines' board (i, j) of
       0 -> ' '
       x -> head (show x)
 
-countMines' :: Pos -> Board -> Int
-countMines' (i, j) board = length $ filter (`isMine` board) indices
+countMines' :: Board -> Pos -> Int
+countMines' board (i, j) = length $ filter (isMine board) indices
   where
     range = [-1 .. 1]
     indices = [(i + di, j + dj) | di <- range, dj <- range]
 
-isMine :: Pos -> Board -> Bool
-isMine (i, j) board
+isMine :: Board -> Pos -> Bool
+isMine board (i, j)
   | i < 0 || i > m || j < 0 || j > n = False
   | otherwise = board !! i !! j == '*'
   where
